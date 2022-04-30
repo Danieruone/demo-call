8b03b7e8873bb2df6aa8ca2fd4c144774223adf9
Daniel <danieruone@gmail.com>
2022-04-29 20:50:43 -0500

    Add controls panel

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,15 +1,13 @@
 // components
 import { UserIcon } from '../Components/UserIcon';
+import { ControlsPanel } from '../Components/ControlsPanel';
 
 export const Main = () => {
   return (
     <div className='container'>
       <UserIcon />
       <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
-      <UserIcon />
+      <ControlsPanel />
     </div>
   );
 };
