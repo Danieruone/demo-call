430da141d97b5782e61128c9071a7d992ec998e8
Daniel <danieruone@gmail.com>
2022-05-02 20:14:37 -0500

    Add create username modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 // pages
+import { Home } from './Pages/Home';
 import { Main } from './Pages/Main';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Main />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/:uuid' element={<Main />} />
     </Routes>
   );
 }
